b8503aabf082d8428942aeefa9cf3cb89bdf6b40
Alexandre Gattiker <algattik@users.noreply.github.com>
2022-06-14 07:36:51 +0200

    Feature/226 checkstyle (#4)

--- extensions/identity-hub/src/main/java/org/eclipse/dataspaceconnector/dtos/FeatureDetection.java
+++ extensions/identity-hub/src/main/java/org/eclipse/dataspaceconnector/dtos/FeatureDetection.java
@@ -1,3 +1,17 @@
+/*
+ *  Copyright (c) 2022 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Microsoft Corporation - initial API and implementation
+ *
+ */
+
 package org.eclipse.dataspaceconnector.dtos;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
@@ -13,12 +27,12 @@ import java.util.List;
  */
 @JsonDeserialize(builder = FeatureDetection.Builder.class)
 public class FeatureDetection {
-    private static final String type = "FeatureDetection";
+    private static final String TYPE = "FeatureDetection";
 
     private List<WebNodeInterface> interfaces;
 
     public String getType() {
-        return type;
+        return TYPE;
     }
 
     public List<WebNodeInterface> getInterfaces() {
