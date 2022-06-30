7ede4ef29744609027fa3d873d1434007be9639e
Ophelie Le Mentec <17216799+ouphi@users.noreply.github.com>
2022-06-30 17:14:08 +0200

    Removed MessageStatus builder.

--- spi/identity-hub-spi/src/main/java/org/eclipse/dataspaceconnector/identityhub/models/MessageStatus.java
+++ spi/identity-hub-spi/src/main/java/org/eclipse/dataspaceconnector/identityhub/models/MessageStatus.java
@@ -14,51 +14,18 @@
 
 package org.eclipse.dataspaceconnector.identityhub.models;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
-
-import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * See <a href="https://identity.foundation/decentralized-web-node/spec/#response-objects">response objects documentation</a>
  * and <a href="https://identity.foundation/decentralized-web-node/spec/#message-level-status-coding">status doc</a>.
  */
-@JsonDeserialize(builder = MessageStatus.Builder.class)
 public class MessageStatus extends Status {
     public static final MessageStatus OK = new MessageStatus(200, "The message was successfully processed");
     public static final MessageStatus MALFORMED_MESSAGE = new MessageStatus(400, "The message was malformed or improperly constructed");
     public static final MessageStatus INTERFACE_NOT_IMPLEMENTED = new MessageStatus(501, "The interface method is not implemented");
 
-    private MessageStatus(int code, String detail) {
+    private MessageStatus(@JsonProperty("code") int code, @JsonProperty("detail") String detail) {
         super(code, detail);
-    }
-
-    @JsonPOJOBuilder(withPrefix = "")
-    public static final class Builder {
-        private Integer code;
-        private String detail;
-
-        private Builder() {
-        }
-
-        public static Builder newInstance() {
-            return new Builder();
-        }
-
-        public Builder code(int code) {
-            this.code = code;
-            return this;
-        }
-
-        public Builder detail(String detail) {
-            this.detail = detail;
-            return this;
-        }
-
-        public MessageStatus build() {
-            Objects.requireNonNull(code, "MessageStatus must contain code property.");
-            Objects.requireNonNull(detail, "MessageStatus must contain detail property.");
-            return new MessageStatus(code, detail);
-        }
     }
 }
