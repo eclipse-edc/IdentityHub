1d95d31d63ccb82d64744dc509d1ad4b191396d9
Ophelie Le Mentec <17216799+ouphi@users.noreply.github.com>
2022-07-19 17:34:18 +0200

    Fixed checkstyle.

--- identity-hub-junit/src/main/java/org/eclipse/dataspaceconnector/identityhub/junit/testfixtures/VerifiableCredentialTestUtil.java
+++ identity-hub-junit/src/main/java/org/eclipse/dataspaceconnector/identityhub/junit/testfixtures/VerifiableCredentialTestUtil.java
@@ -15,7 +15,6 @@
 package org.eclipse.dataspaceconnector.identityhub.junit.testfixtures;
 
 import com.github.javafaker.Faker;
-import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.crypto.ECDSASigner;
@@ -23,7 +22,6 @@ import com.nimbusds.jose.jwk.Curve;
 import com.nimbusds.jose.jwk.ECKey;
 import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.jwk.gen.ECKeyGenerator;
-import com.nimbusds.jose.jwk.gen.JWKGenerator;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import org.eclipse.dataspaceconnector.iam.did.crypto.key.KeyConverter;
