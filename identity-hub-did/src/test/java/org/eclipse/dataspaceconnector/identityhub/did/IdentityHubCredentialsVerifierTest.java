9cab7d94945594e9c0a3f4ba90728ebd289f3789
Ophelie Le Mentec <17216799+ouphi@users.noreply.github.com>
2022-07-20 10:02:34 +0200

    Move common vars and services to the top.

--- identity-hub-did/src/test/java/org/eclipse/dataspaceconnector/identityhub/did/IdentityHubCredentialsVerifierTest.java
+++ identity-hub-did/src/test/java/org/eclipse/dataspaceconnector/identityhub/did/IdentityHubCredentialsVerifierTest.java
@@ -19,6 +19,7 @@ import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
+import org.eclipse.dataspaceconnector.iam.did.spi.credentials.CredentialsVerifier;
 import org.eclipse.dataspaceconnector.iam.did.spi.document.DidDocument;
 import org.eclipse.dataspaceconnector.iam.did.spi.document.Service;
 import org.eclipse.dataspaceconnector.identityhub.client.IdentityHubClient;
@@ -26,6 +27,7 @@ import org.eclipse.dataspaceconnector.spi.monitor.ConsoleMonitor;
 import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
 import org.eclipse.dataspaceconnector.spi.response.ResponseStatus;
 import org.eclipse.dataspaceconnector.spi.response.StatusResult;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -41,16 +43,24 @@ public class IdentityHubCredentialsVerifierTest {
 
     private static final Faker FAKER = new Faker();
     private static final Monitor MONITOR = new ConsoleMonitor();
+    private static IdentityHubClient identityHubClient;
+    private static SignatureVerifier signatureVerifier;
+    private static CredentialsVerifier credentialsVerifier;
+    private static String hubBaseUrl;
+
+    @BeforeEach
+    void setUp() {
+        identityHubClient = mock(IdentityHubClient.class);
+        signatureVerifier = mock(SignatureVerifier.class);
+        credentialsVerifier = new IdentityHubCredentialsVerifier(identityHubClient, MONITOR, signatureVerifier);
+        hubBaseUrl = "https://" + FAKER.internet().url();
+    }
 
     @Test
     public void getVerifiedClaims_getValidClaims() throws Exception {
 
         // Arrange
-        var hubBaseUrl = "https://" + FAKER.internet().url();
         var issuer = FAKER.internet().url();
-        var identityHubClient = mock(IdentityHubClient.class);
-        var signatureVerifier = mock(SignatureVerifier.class);
-        var credentialsVerifier = new IdentityHubCredentialsVerifier(identityHubClient, MONITOR, signatureVerifier);
         var didDocument = DidDocument.Builder.newInstance()
                 .service(List.of(new Service("IdentityHub", "IdentityHub", hubBaseUrl))).build();
         var credential = generateVerifiableCredential();
@@ -73,11 +83,7 @@ public class IdentityHubCredentialsVerifierTest {
     public void getVerifiedClaims_filtersSignedByWrongIssuer() throws Exception {
 
         // Arrange
-        var hubBaseUrl = "https://" + FAKER.internet().url();
         var issuer = FAKER.internet().url();
-        var identityHubClient = mock(IdentityHubClient.class);
-        var signatureVerifier = mock(SignatureVerifier.class);
-        var credentialsVerifier = new IdentityHubCredentialsVerifier(identityHubClient, MONITOR, signatureVerifier);
         var didDocument = DidDocument.Builder.newInstance()
                 .service(List.of(new Service("IdentityHub", "IdentityHub", hubBaseUrl))).build();
         var credential = generateVerifiableCredential();
@@ -97,7 +103,6 @@ public class IdentityHubCredentialsVerifierTest {
     @Test
     public void getVerifiedClaims_hubUrlNotResolved() {
         // Arrange
-        var credentialsVerifier = new IdentityHubCredentialsVerifier(mock(IdentityHubClient.class), MONITOR, mock(SignatureVerifier.class));
         var didDocument = DidDocument.Builder.newInstance().build();
 
         // Act
@@ -111,9 +116,6 @@ public class IdentityHubCredentialsVerifierTest {
     public void getVerifiedClaims_idHubCallFails() {
 
         // Arrange
-        var hubBaseUrl = "https://" + FAKER.internet().url();
-        var identityHubClient = mock(IdentityHubClient.class);
-        var credentialsVerifier = new IdentityHubCredentialsVerifier(identityHubClient, MONITOR, mock(SignatureVerifier.class));
         var didDocument = DidDocument.Builder.newInstance().service(List.of(new Service("IdentityHub", "IdentityHub", hubBaseUrl))).build();
         when(identityHubClient.getVerifiableCredentials(hubBaseUrl)).thenReturn(StatusResult.failure(ResponseStatus.FATAL_ERROR));
 
@@ -128,10 +130,6 @@ public class IdentityHubCredentialsVerifierTest {
     public void getVerifiedClaims_verifiableCredentialsWithWrongFormat() {
 
         // Arrange
-        var hubBaseUrl = "https://" + FAKER.internet().url();
-        var identityHubClient = mock(IdentityHubClient.class);
-        var signatureVerifier = mock(SignatureVerifier.class);
-        var credentialsVerifier = new IdentityHubCredentialsVerifier(identityHubClient, MONITOR, signatureVerifier);
         var didDocument = DidDocument.Builder.newInstance()
                 .service(List.of(new Service("IdentityHub", "IdentityHub", hubBaseUrl))).build();
         var jws = new SignedJWT(new JWSHeader.Builder(JWSAlgorithm.ES256).build(), new JWTClaimsSet.Builder().build());
