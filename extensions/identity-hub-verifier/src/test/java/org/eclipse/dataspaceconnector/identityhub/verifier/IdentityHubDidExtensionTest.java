bee5d2f3574f46cee6e050b7aee9cf2a84ad2159
Marc Gomez <marcgomez@microsoft.com>
2022-07-21 15:00:11 +0200

    Constants cleanup

--- extensions/identity-hub-verifier/src/test/java/org/eclipse/dataspaceconnector/identityhub/verifier/IdentityHubDidExtensionTest.java
+++ extensions/identity-hub-verifier/src/test/java/org/eclipse/dataspaceconnector/identityhub/verifier/IdentityHubDidExtensionTest.java
@@ -26,7 +26,6 @@ import org.eclipse.dataspaceconnector.identityhub.client.IdentityHubClientImpl;
 import org.eclipse.dataspaceconnector.junit.extensions.EdcExtension;
 import org.eclipse.dataspaceconnector.junit.testfixtures.TestUtils;
 import org.eclipse.dataspaceconnector.spi.monitor.ConsoleMonitor;
-import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
 import org.eclipse.dataspaceconnector.spi.result.Result;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -48,19 +47,17 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(EdcExtension.class)
 public class IdentityHubDidExtensionTest {
+    private static final Faker FAKER = new Faker();
     private static final int PORT = getFreePort();
     private static final String API_URL = String.format("http://localhost:%d/api/identity-hub", PORT);
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-    private static final Faker FAKER = new Faker();
-    private static final Monitor MONITOR = new ConsoleMonitor();
+    private static final String CREDENTIAL_ISSUER = FAKER.internet().url();
+    private static final String SUBJECT = FAKER.internet().url();
     private IdentityHubClient identityHubClient;
     private DidPublicKeyResolver publicKeyResolver;
-    String credentialIssuer = FAKER.internet().url();
-    String subject = FAKER.internet().url();
 
     @BeforeEach
     void setUp(EdcExtension extension) {
-        identityHubClient = new IdentityHubClientImpl(TestUtils.testOkHttpClient(), OBJECT_MAPPER, MONITOR);
+        identityHubClient = new IdentityHubClientImpl(TestUtils.testOkHttpClient(), new ObjectMapper(), new ConsoleMonitor());
         publicKeyResolver = mock(DidPublicKeyResolverImpl.class);
         extension.registerServiceMock(DidPublicKeyResolver.class, publicKeyResolver);
         extension.setConfiguration(Map.of("web.http.port", String.valueOf(PORT), "edc.identity.hub.url", API_URL));
@@ -74,16 +71,16 @@ public class IdentityHubDidExtensionTest {
         var jwk = generateEcKey();
         when(publicKeyResolver.resolvePublicKey(anyString())).thenReturn(Result.success(toPublicKeyWrapper(jwk)));
         var didDocument = DidDocument.Builder.newInstance()
-                .id(subject)
+                .id(SUBJECT)
                 .service(List.of(new Service("IdentityHub", "IdentityHub", API_URL)))
                 .build();
         var credential = generateVerifiableCredential();
-        var jwt = buildSignedJwt(credential, credentialIssuer, subject, jwk);
+        var jwt = buildSignedJwt(credential, CREDENTIAL_ISSUER, SUBJECT, jwk);
 
         // Act
         identityHubClient.addVerifiableCredential(API_URL, jwt);
         var credentials = verifier.getVerifiedCredentials(didDocument);
-        var expectedCredentials = toMap(credential, credentialIssuer, subject);
+        var expectedCredentials = toMap(credential, CREDENTIAL_ISSUER, SUBJECT);
 
         // Assert
         assertThat(credentials.succeeded());
