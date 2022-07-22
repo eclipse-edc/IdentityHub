894e7c40e0d58f8d6c3eea11e524cf93214d8a71
Marc Gomez <marcgomez@microsoft.com>
2022-07-22 14:27:04 +0200

    Intermediate work on E2E tests

---
+++ system-tests/tests/build.gradle.kts
@@ -0,0 +1,50 @@
+/*
+ *  Copyright (c) 2022 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Microsoft Corporation - initial API and implementation
+ *       Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+ *
+ */
+
+plugins {
+    `java-library`
+}
+
+val edcVersion: String by project
+val edcGroup: String by project
+val jacksonVersion: String by project
+val jupiterVersion: String by project
+val assertj: String by project
+val mockitoVersion: String by project
+val faker: String by project
+val okHttpVersion: String by project
+val nimbusVersion: String by project
+val bouncycastleVersion: String by project
+
+dependencies {
+    testImplementation(project(":system-tests:launcher"))
+    testImplementation(project(":spi:identity-hub-spi"))
+    testImplementation(project(":extensions:identity-hub-verifier"))
+    testImplementation(project(":client-cli"))
+    testImplementation("${edcGroup}:identity-did-core:${edcVersion}")
+    testImplementation("${edcGroup}:junit:${edcVersion}")
+    testImplementation("info.picocli:picocli:4.6.3")
+    testImplementation("info.picocli:picocli-codegen:4.6.3")
+    testImplementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+    testImplementation("com.squareup.okhttp3:okhttp:${okHttpVersion}")
+    testImplementation("com.nimbusds:nimbus-jose-jwt:${nimbusVersion}")
+    testImplementation("org.bouncycastle:bcpkix-jdk15on:${bouncycastleVersion}")
+    testImplementation("org.assertj:assertj-core:${assertj}")
+    testImplementation("org.mockito:mockito-core:${mockitoVersion}")
+    testImplementation("com.github.javafaker:javafaker:${faker}")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${jupiterVersion}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${jupiterVersion}")
+}
+
---
+++ system-tests/tests/src/test/java/org/eclipse/dataspaceconnector/identityhub/systemtests/E2ETest.java
@@ -0,0 +1,85 @@
+/*
+ *  Copyright (c) 2022 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Microsoft Corporation - initial API and implementation
+ *
+ */
+
+package org.eclipse.dataspaceconnector.identityhub.systemtests;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
+import org.eclipse.dataspaceconnector.iam.did.spi.credentials.CredentialsVerifier;
+import org.eclipse.dataspaceconnector.iam.did.spi.resolution.DidResolverRegistry;
+import org.eclipse.dataspaceconnector.identityhub.cli.IdentityHubCli;
+import org.eclipse.dataspaceconnector.identityhub.credentials.model.VerifiableCredential;
+import org.eclipse.dataspaceconnector.identityhub.verifier.IdentityHubCredentialsVerifier;
+import org.eclipse.dataspaceconnector.junit.extensions.EdcExtension;
+import org.eclipse.dataspaceconnector.spi.result.Result;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import picocli.CommandLine;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(EdcExtension.class)
+class E2ETest {
+
+    static final Faker FAKER = new Faker();
+    static final String HUB_URL = "http://localhost:8181/api/identity-hub";
+    static final ObjectMapper MAPPER = new ObjectMapper();
+
+    static final VerifiableCredential VC1 = VerifiableCredential.Builder.newInstance()
+            .id(FAKER.internet().uuid())
+            .credentialSubject(Map.of(
+                    FAKER.internet().uuid(), FAKER.lorem().word(),
+                    FAKER.internet().uuid(), FAKER.lorem().word()))
+            .build();
+
+    IdentityHubCredentialsVerifier verifier;
+
+    IdentityHubCli app = new IdentityHubCli();
+    CommandLine cmd = IdentityHubCli.getCommandLine();
+    StringWriter out = new StringWriter();
+    StringWriter err = new StringWriter();
+
+    @BeforeEach
+    void setUp(EdcExtension extension) {
+        cmd.setOut(new PrintWriter(out));
+        cmd.setErr(new PrintWriter(err));
+
+        extension.setConfiguration(Map.of("web.http.port", "8081", "edc.identity.hub.url", HUB_URL));
+    }
+
+    @Test
+    void list(CredentialsVerifier verifier, DidResolverRegistry resolverRegistry) throws Exception {
+        int result = cmd.execute("-s", HUB_URL, "vc", "list");
+        assertThat(result).isEqualTo(0);
+
+        var claims = MAPPER.readValue(out.toString(), new TypeReference<List<Map<String, Object>>>() {});
+        assertThat(claims).describedAs("Identity Hub already contains Verifiable Credentials").isEmpty();
+
+        var json = MAPPER.writeValueAsString(VC1);
+        cmd.execute("-s", HUB_URL, "vc", "add", "-c", json, "-i", "did:web:did-server:authority", "-b", "did:web:did-server:identity-hub-owner", "-k", "resources/jwt/authority/private-key.pem");
+
+        var didResult = resolverRegistry.resolve("did:web:did-server:identity-hub-owner");
+        assertThat(didResult.succeeded()).isTrue();
+
+        Result<Map<String, Object>> verifiedCredentials = verifier.getVerifiedCredentials(didResult.getContent());
+        System.out.println(verifiedCredentials);
+    }
+}
