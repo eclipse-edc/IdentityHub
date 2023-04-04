0c35585fa611982b4e9384bd93cb7eb20b1d2eea
ndr_brt <andrea.bertagnolli@gmail.com>
2023-04-04 16:19:40 +0200

    fix: compilation issue (#97)

--- extensions/credentials/identity-hub-credentials-jwt/build.gradle.kts
+++ extensions/credentials/identity-hub-credentials-jwt/build.gradle.kts
@@ -22,8 +22,7 @@ dependencies {
 
     implementation(libs.edc.spi.identity.did)
     implementation(root.nimbus.jwt)
-    implementation(root.okhttp)
-    
+
     testImplementation(testFixtures(project(":spi:identity-hub-spi")))
     testImplementation(project(":extensions:identity-hub-api"))
     testImplementation(libs.edc.ext.identity.did.core)
