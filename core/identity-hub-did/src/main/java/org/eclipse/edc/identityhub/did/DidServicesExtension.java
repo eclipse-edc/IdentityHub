9be9686737bfc290c69366daa618654d46113b2b
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2023-12-07 08:50:50 +0100

    pr remark

--- core/identity-hub-did/src/main/java/org/eclipse/edc/identityhub/did/DidServicesExtension.java
+++ core/identity-hub-did/src/main/java/org/eclipse/edc/identityhub/did/DidServicesExtension.java
@@ -14,5 +14,12 @@
 
 package org.eclipse.edc.identityhub.did;
 
-public class DidServicesExtension {
+import org.eclipse.edc.runtime.metamodel.annotation.Extension;
+import org.eclipse.edc.spi.system.ServiceExtension;
+
+import static org.eclipse.edc.identityhub.did.DidServicesExtension.NAME;
+
+@Extension(value = NAME)
+public class DidServicesExtension implements ServiceExtension {
+    public static final String NAME = "DID Service Extension";
 }
