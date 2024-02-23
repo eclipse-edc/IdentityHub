159eb0f9ac2665d59bf35b9849c3a244cf53265a
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2024-02-23 10:06:54 +0100

    fix: register sql keypairesource store (#281)
    * fix: register SQL KeyPairResource store
    
    * DEPENDENCIES

--- extensions/store/sql/identity-hub-keypair-store-sql/src/main/java/org/eclipse/edc/identityhub/store/sql/keypair/SqlKeyPairResourceStoreExtension.java
+++ extensions/store/sql/identity-hub-keypair-store-sql/src/main/java/org/eclipse/edc/identityhub/store/sql/keypair/SqlKeyPairResourceStoreExtension.java
@@ -14,7 +14,59 @@
 
 package org.eclipse.edc.identityhub.store.sql.keypair;
 
+import org.eclipse.edc.identityhub.spi.store.KeyPairResourceStore;
+import org.eclipse.edc.identityhub.store.sql.keypair.schema.postgres.PostgresDialectStatements;
+import org.eclipse.edc.runtime.metamodel.annotation.Extension;
+import org.eclipse.edc.runtime.metamodel.annotation.Inject;
+import org.eclipse.edc.runtime.metamodel.annotation.Provider;
+import org.eclipse.edc.runtime.metamodel.annotation.Setting;
 import org.eclipse.edc.spi.system.ServiceExtension;
+import org.eclipse.edc.spi.system.ServiceExtensionContext;
+import org.eclipse.edc.spi.types.TypeManager;
+import org.eclipse.edc.sql.QueryExecutor;
+import org.eclipse.edc.transaction.datasource.spi.DataSourceRegistry;
+import org.eclipse.edc.transaction.spi.TransactionContext;
 
+import static org.eclipse.edc.identityhub.store.sql.keypair.SqlKeyPairResourceStoreExtension.NAME;
+
+@Extension(NAME)
 public class SqlKeyPairResourceStoreExtension implements ServiceExtension {
+    public static final String NAME = "KeyPair Resource SQL Store Extension";
+
+    @Setting(value = "Datasource name for the KeyPairResource database", defaultValue = DataSourceRegistry.DEFAULT_DATASOURCE)
+    public static final String DATASOURCE_SETTING_NAME = "edc.datasource.keypair.name";
+
+    @Inject
+    private DataSourceRegistry dataSourceRegistry;
+
+    @Inject
+    private TransactionContext transactionContext;
+
+    @Inject
+    private TypeManager typemanager;
+
+    @Inject
+    private QueryExecutor queryExecutor;
+
+    @Inject(required = false)
+    private KeyPairResourceStoreStatements statements;
+
+    @Override
+    public String name() {
+        return NAME;
+    }
+
+    @Provider
+    public KeyPairResourceStore createSqlStore(ServiceExtensionContext context) {
+        return new SqlKeyPairResourceStore(dataSourceRegistry, getDataSourceName(context), transactionContext, typemanager.getMapper(),
+                queryExecutor, getStatementImpl());
+    }
+
+    private KeyPairResourceStoreStatements getStatementImpl() {
+        return statements != null ? statements : new PostgresDialectStatements();
+    }
+
+    private String getDataSourceName(ServiceExtensionContext context) {
+        return context.getConfig().getString(DATASOURCE_SETTING_NAME, DataSourceRegistry.DEFAULT_DATASOURCE);
+    }
 }
