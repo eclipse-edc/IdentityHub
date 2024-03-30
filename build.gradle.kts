5f421e89e382b01f15d8e16d8bea6be6a93c134c
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2024-03-30 12:20:40 +0100

    fix: compile errors after EDC refactor (#311)
    * chore(compile): fix compile errors after EDC refactor
    
    * DEPENDENCIES

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,10 +59,4 @@ allprojects {
         configDirectory.set(rootProject.file("resources"))
     }
 
-    // EdcRuntimeExtension uses this to determine the runtime classpath of the module to run.
-    tasks.register("printClasspath") {
-        doLast {
-            println(sourceSets["main"].runtimeClasspath.asPath)
-        }
-    }
 }
