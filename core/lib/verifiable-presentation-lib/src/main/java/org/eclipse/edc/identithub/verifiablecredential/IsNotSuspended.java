b161ace8329082ba063d38a45d6a93854abb5204
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2024-05-13 12:09:07 +0200

    wip not suspended

---
+++ core/lib/verifiable-presentation-lib/src/main/java/org/eclipse/edc/identithub/verifiablecredential/IsNotSuspended.java
@@ -0,0 +1,30 @@
+/*
+ *  Copyright (c) 2024 Metaform Systems, Inc.
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Metaform Systems, Inc. - initial API and implementation
+ *
+ */
+
+package org.eclipse.edc.identithub.verifiablecredential;
+
+import org.eclipse.edc.iam.verifiablecredentials.spi.RevocationListService;
+import org.eclipse.edc.iam.verifiablecredentials.spi.model.VerifiableCredential;
+import org.eclipse.edc.iam.verifiablecredentials.spi.validation.CredentialValidationRule;
+import org.eclipse.edc.spi.result.Result;
+
+public class IsNotSuspended implements CredentialValidationRule {
+    public IsNotSuspended(RevocationListService revocationListService) {
+    }
+
+    @Override
+    public Result<Void> apply(VerifiableCredential verifiableCredential) {
+        return null;
+    }
+}
