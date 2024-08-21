b0ba1f6df4533872688df3fb399f309f3200b3f0
ndr_brt <andrea.bertagnolli@gmail.com>
2024-08-21 18:05:50 +0200

    ci: launch test workflow also on release and bugfix branches (#427)
    * ci: launch test workflow also on release and bugfix branches
    
    * DEPENDENCIES

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,39 +18,21 @@ plugins {
 
 val edcScmConnection: String by project
 val edcScmUrl: String by project
-val annotationProcessorVersion: String by project
-val metaModelVersion: String by project
 
 buildscript {
     dependencies {
-        val edcGradlePluginsVersion: String by project
-        classpath("org.eclipse.edc.edc-build:org.eclipse.edc.edc-build.gradle.plugin:${edcGradlePluginsVersion}")
+        val version: String by project
+        classpath("org.eclipse.edc.edc-build:org.eclipse.edc.edc-build.gradle.plugin:$version")
     }
 }
 
 allprojects {
     apply(plugin = "${group}.edc-build")
 
-    // configure which version of the annotation processor to use. defaults to the same version as the plugin
-    configure<org.eclipse.edc.plugins.autodoc.AutodocExtension> {
-        processorVersion.set(annotationProcessorVersion)
-        outputDirectory.set(project.layout.buildDirectory.asFile)
-    }
-
     configure<org.eclipse.edc.plugins.edcbuild.extensions.BuildExtension> {
-        versions {
-            // override default dependency versions here
-            metaModel.set(metaModelVersion)
-        }
         pom {
             scmConnection.set(edcScmConnection)
             scmUrl.set(edcScmUrl)
-        }
-        swagger {
-            title.set("Identity Hub REST API")
-            description = "Identity Hub REST APIs - merged by OpenApiMerger"
-            outputFilename.set(project.name)
-            outputDirectory.set(file("${rootProject.projectDir.path}/resources/openapi/yaml"))
         }
     }
 
