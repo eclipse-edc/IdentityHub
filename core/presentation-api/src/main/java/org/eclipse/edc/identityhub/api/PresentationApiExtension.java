38ce63d9761f620e97a8364ced72462ff07df5f1
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2024-08-23 09:30:17 +0200

    register version info with service

--- core/presentation-api/src/main/java/org/eclipse/edc/identityhub/api/PresentationApiExtension.java
+++ core/presentation-api/src/main/java/org/eclipse/edc/identityhub/api/PresentationApiExtension.java
@@ -14,6 +14,7 @@
 
 package org.eclipse.edc.identityhub.api;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import org.eclipse.edc.iam.identitytrust.spi.model.PresentationQueryMessage;
 import org.eclipse.edc.iam.identitytrust.transform.from.JsonObjectFromPresentationResponseMessageTransformer;
 import org.eclipse.edc.iam.identitytrust.transform.to.JsonObjectToPresentationQueryTransformer;
@@ -26,8 +27,11 @@ import org.eclipse.edc.identityhub.spi.verification.AccessTokenVerifier;
 import org.eclipse.edc.jsonld.spi.JsonLd;
 import org.eclipse.edc.runtime.metamodel.annotation.Extension;
 import org.eclipse.edc.runtime.metamodel.annotation.Inject;
+import org.eclipse.edc.spi.EdcException;
 import org.eclipse.edc.spi.system.ServiceExtension;
 import org.eclipse.edc.spi.system.ServiceExtensionContext;
+import org.eclipse.edc.spi.system.apiversion.ApiVersionService;
+import org.eclipse.edc.spi.system.apiversion.VersionRecord;
 import org.eclipse.edc.spi.types.TypeManager;
 import org.eclipse.edc.transform.spi.TypeTransformerRegistry;
 import org.eclipse.edc.transform.transformer.edc.to.JsonValueToGenericTypeTransformer;
@@ -36,8 +40,12 @@ import org.eclipse.edc.web.jersey.providers.jsonld.JerseyJsonLdInterceptor;
 import org.eclipse.edc.web.jersey.providers.jsonld.ObjectMapperProvider;
 import org.eclipse.edc.web.spi.WebService;
 
+import java.io.IOException;
+import java.util.stream.Stream;
+
 import static org.eclipse.edc.iam.identitytrust.spi.DcpConstants.DCP_CONTEXT_URL;
 import static org.eclipse.edc.identityhub.api.PresentationApiExtension.NAME;
+import static org.eclipse.edc.identityhub.spi.IdentityHubApiContext.PRESENTATION;
 import static org.eclipse.edc.spi.constants.CoreConstants.JSON_LD;
 
 @Extension(value = NAME)
@@ -45,7 +53,7 @@ public class PresentationApiExtension implements ServiceExtension {
 
     public static final String NAME = "Presentation API Extension";
     public static final String RESOLUTION_SCOPE = "resolution-scope";
-    public static final String RESOLUTION_CONTEXT = "resolution";
+    private static final String API_VERSION_JSON_FILE = "presentation-api-version.json";
     @Inject
     private TypeTransformerRegistry typeTransformer;
     @Inject
@@ -64,6 +72,8 @@ public class PresentationApiExtension implements ServiceExtension {
     private TypeManager typeManager;
     @Inject
     private ParticipantContextService participantContextService;
+    @Inject
+    private ApiVersionService apiVersionService;
 
     @Override
     public String name() {
@@ -79,9 +89,9 @@ public class PresentationApiExtension implements ServiceExtension {
         var controller = new PresentationApiController(validatorRegistry, typeTransformer, credentialResolver, accessTokenVerifier, verifiablePresentationService, context.getMonitor(), participantContextService);
 
         var jsonLdMapper = typeManager.getMapper(JSON_LD);
-        webService.registerResource(RESOLUTION_CONTEXT, new ObjectMapperProvider(jsonLdMapper));
-        webService.registerResource(RESOLUTION_CONTEXT, new JerseyJsonLdInterceptor(jsonLd, jsonLdMapper, RESOLUTION_SCOPE));
-        webService.registerResource(RESOLUTION_CONTEXT, controller);
+        webService.registerResource(PRESENTATION, new ObjectMapperProvider(jsonLdMapper));
+        webService.registerResource(PRESENTATION, new JerseyJsonLdInterceptor(jsonLd, jsonLdMapper, RESOLUTION_SCOPE));
+        webService.registerResource(PRESENTATION, controller);
 
         jsonLd.registerContext(DCP_CONTEXT_URL, RESOLUTION_SCOPE);
 
@@ -89,7 +99,22 @@ public class PresentationApiExtension implements ServiceExtension {
         typeTransformer.register(new JsonObjectToPresentationQueryTransformer(jsonLdMapper));
         typeTransformer.register(new JsonValueToGenericTypeTransformer(jsonLdMapper));
         typeTransformer.register(new JsonObjectFromPresentationResponseMessageTransformer());
+
+        registerVersionInfo(getClass().getClassLoader());
     }
 
+    private void registerVersionInfo(ClassLoader resourceClassLoader) {
+        try (var versionContent = resourceClassLoader.getResourceAsStream(API_VERSION_JSON_FILE)) {
+            if (versionContent == null) {
+                throw new EdcException("Version file not found or not readable.");
+            }
+            Stream.of(typeManager.getMapper()
+                            .enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+                            .readValue(versionContent, VersionRecord[].class))
+                    .forEach(vr -> apiVersionService.addRecord("presentation", vr));
+        } catch (IOException e) {
+            throw new EdcException(e);
+        }
+    }
 
 }
