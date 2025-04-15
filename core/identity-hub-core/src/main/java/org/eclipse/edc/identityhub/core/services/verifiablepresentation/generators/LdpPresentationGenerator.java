ca3e2a8f4d5f27afc053a74718642e595b9e624b
u-veys <ueveys.ok@gmail.com>
2025-04-15 09:55:17 +0200

    fix: prevent 'invalid Uri' error (#689)
    * fix: prevent 'invalid Uri' error when creating verificationMethod for proofDraft
    
    * test: add test for 'invalid Uri' error when creating verificationMethod for proofDraft
    
    * fix: add variable to compose controller and keyId

--- core/identity-hub-core/src/main/java/org/eclipse/edc/identityhub/core/services/verifiablepresentation/generators/LdpPresentationGenerator.java
+++ core/identity-hub-core/src/main/java/org/eclipse/edc/identityhub/core/services/verifiablepresentation/generators/LdpPresentationGenerator.java
@@ -159,7 +159,12 @@ public class LdpPresentationGenerator implements PresentationGenerator<JsonObjec
     }
 
     private JsonObject signPresentation(JsonObject presentationObject, SignatureSuite suite, String suiteIdentifier, PrivateKey pk, String publicKeyId, String controller) {
-        var keyIdUri = URI.create(publicKeyId);
+        var composedKeyId = publicKeyId;
+        if (!publicKeyId.startsWith(controller)) {
+            composedKeyId = controller + "#" + publicKeyId;
+        }
+
+        var keyIdUri = URI.create(composedKeyId);
         var controllerUri = URI.create(controller);
         var verificationMethodType = URI.create(suiteIdentifier);
 
@@ -169,7 +174,7 @@ public class LdpPresentationGenerator implements PresentationGenerator<JsonObjec
 
         var proofDraft = Jws2020ProofDraft.Builder.newInstance()
                 .proofPurpose(ASSERTION_METHOD)
-                .verificationMethod(new JsonWebKeyPair(URI.create(controller + "#" + publicKeyId), verificationMethodType, controllerUri, null))
+                .verificationMethod(new JsonWebKeyPair(keyIdUri, verificationMethodType, controllerUri, null))
                 .created(Instant.now())
                 .mapper(typeManager.getMapper(typeContext))
                 .build();
