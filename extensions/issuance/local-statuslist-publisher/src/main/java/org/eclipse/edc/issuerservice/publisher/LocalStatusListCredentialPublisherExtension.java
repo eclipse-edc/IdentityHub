db246bd039208a41c3e2cae99e46892bf52edcee
Benjamin SCHOLTES <benjamin.scholtes@amadeus.com>
2025-04-16 15:56:04 +0200

    fix: use mapper embedding date/time modules in LocalStatusListCredentialPublisherExtension

--- extensions/issuance/local-statuslist-publisher/src/main/java/org/eclipse/edc/issuerservice/publisher/LocalStatusListCredentialPublisherExtension.java
+++ extensions/issuance/local-statuslist-publisher/src/main/java/org/eclipse/edc/issuerservice/publisher/LocalStatusListCredentialPublisherExtension.java
@@ -14,7 +14,6 @@
 
 package org.eclipse.edc.issuerservice.publisher;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.edc.identityhub.spi.verifiablecredentials.store.CredentialStore;
 import org.eclipse.edc.issuerservice.publisher.api.StatusListCredentialController;
 import org.eclipse.edc.issuerservice.spi.credentials.statuslist.StatusListCredentialPublisher;
@@ -24,9 +23,11 @@ import org.eclipse.edc.runtime.metamodel.annotation.Inject;
 import org.eclipse.edc.runtime.metamodel.annotation.Provider;
 import org.eclipse.edc.runtime.metamodel.annotation.Setting;
 import org.eclipse.edc.runtime.metamodel.annotation.Settings;
+import org.eclipse.edc.spi.monitor.Monitor;
 import org.eclipse.edc.spi.system.Hostname;
 import org.eclipse.edc.spi.system.ServiceExtension;
 import org.eclipse.edc.spi.system.ServiceExtensionContext;
+import org.eclipse.edc.spi.types.TypeManager;
 import org.eclipse.edc.transaction.spi.TransactionContext;
 import org.eclipse.edc.web.spi.WebService;
 import org.eclipse.edc.web.spi.configuration.PortMapping;
@@ -34,41 +35,49 @@ import org.eclipse.edc.web.spi.configuration.PortMappingRegistry;
 import org.jetbrains.annotations.NotNull;
 
 import static java.util.Optional.ofNullable;
+import static org.eclipse.edc.spi.constants.CoreConstants.JSON_LD;
 
 
 @Extension(value = LocalStatusListCredentialPublisherExtension.NAME)
 public class LocalStatusListCredentialPublisherExtension implements ServiceExtension {
     public static final String NAME = "IssuerService Default Services Extension";
     private static final String STATUS_LIST = "statuslist";
+
     @Inject
     private CredentialStore store;
 
     @Inject
     private PortMappingRegistry portMappingRegistry;
+
     @Inject
     private Hostname hostname;
 
     @Inject
     private WebService webServer;
 
+    @Inject
+    private Monitor monitor;
+
+    @Inject
+    private TypeManager typeManager;
+
     @Configuration
     private StatusListCredentialEndpointConfig config;
     @Inject
     private TransactionContext transactionContext;
+    @Setting(description = "Configures endpoint for reaching the StatusList API.", key = "edc.statuslist.callback.address", required = false)
+    private String callbackAddress;
 
     @Override
     public String name() {
         return NAME;
     }
 
-    @Setting(description = "Configures endpoint for reaching the StatusList API.", key = "edc.statuslist.callback.address", required = false)
-    private String callbackAddress;
-
     @Override
     public void initialize(ServiceExtensionContext context) {
         portMappingRegistry.register(new PortMapping(STATUS_LIST, config.port(), config.path()));
 
-        webServer.registerResource(STATUS_LIST, new StatusListCredentialController(store, context.getMonitor(), new ObjectMapper()));
+        webServer.registerResource(STATUS_LIST, new StatusListCredentialController(store, monitor, () -> typeManager.getMapper(JSON_LD)));
     }
 
     @Provider(isDefault = true)
