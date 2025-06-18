3f197201b00df9cda7dbcb80e2efe36d039e0fbb
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2025-06-18 07:35:14 +0200

    feat: prohibit empty scope array (#745)
    * feat: prohibit empty scope array
    
    * run ci

--- protocols/dcp/dcp-identityhub/presentation-api/src/test/java/org/eclipse/edc/identityservice/api/validation/PresentationQueryValidatorTest.java
+++ protocols/dcp/dcp-identityhub/presentation-api/src/test/java/org/eclipse/edc/identityservice/api/validation/PresentationQueryValidatorTest.java
@@ -19,7 +19,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.json.JsonArray;
 import jakarta.json.JsonObject;
-import org.eclipse.edc.iam.identitytrust.spi.model.PresentationQueryMessage;
+import org.eclipse.edc.iam.identitytrust.spi.DcpConstants;
 import org.eclipse.edc.iam.verifiablecredentials.spi.model.presentationdefinition.Constraints;
 import org.eclipse.edc.iam.verifiablecredentials.spi.model.presentationdefinition.Field;
 import org.eclipse.edc.iam.verifiablecredentials.spi.model.presentationdefinition.InputDescriptor;
@@ -28,6 +28,7 @@ import org.eclipse.edc.identityhub.api.validation.PresentationQueryValidator;
 import org.eclipse.edc.jsonld.TitaniumJsonLd;
 import org.eclipse.edc.jsonld.spi.JsonLd;
 import org.eclipse.edc.jsonld.spi.JsonLdKeywords;
+import org.eclipse.edc.jsonld.spi.JsonLdNamespace;
 import org.eclipse.edc.jsonld.util.JacksonJsonLd;
 import org.junit.jupiter.api.Test;
 
@@ -36,23 +37,33 @@ import java.util.UUID;
 
 import static jakarta.json.Json.createArrayBuilder;
 import static jakarta.json.Json.createObjectBuilder;
-import static org.eclipse.edc.iam.identitytrust.spi.DcpConstants.DSPACE_DCP_NAMESPACE_V_0_8;
+import static org.eclipse.edc.iam.identitytrust.spi.model.PresentationQueryMessage.PRESENTATION_QUERY_MESSAGE_DEFINITION_TERM;
+import static org.eclipse.edc.iam.identitytrust.spi.model.PresentationQueryMessage.PRESENTATION_QUERY_MESSAGE_SCOPE_TERM;
 import static org.eclipse.edc.junit.assertions.AbstractResultAssert.assertThat;
 import static org.mockito.Mockito.mock;
 
 class PresentationQueryValidatorTest {
     public static final ObjectMapper MAPPER = JacksonJsonLd.createObjectMapper();
-    private final PresentationQueryValidator validator = new PresentationQueryValidator(DSPACE_DCP_NAMESPACE_V_0_8);
     private final JsonLd jsonLd = new TitaniumJsonLd(mock());
-
+    private final JsonLdNamespace namespace = DcpConstants.DSPACE_DCP_NAMESPACE_V_1_0;
+    private final PresentationQueryValidator validator = new PresentationQueryValidator(namespace);
 
     @Test
     void validate_withScope_success() {
         var jo = createObjectBuilder()
-                .add(PresentationQueryMessage.PRESENTATION_QUERY_MESSAGE_SCOPE_PROPERTY, createScopeArray())
+                .add(namespace.toIri(PRESENTATION_QUERY_MESSAGE_DEFINITION_TERM), createScopeArray())
                 .build();
 
         assertThat(validator.validate(jo)).isSucceeded();
+    }
+
+    @Test
+    void validate_withEmptyScopeArray_fails() {
+        var jo = createObjectBuilder()
+                .add(namespace.toIri(PRESENTATION_QUERY_MESSAGE_SCOPE_TERM), createArrayBuilder().build())
+                .build();
+
+        assertThat(validator.validate(jo)).isFailed().detail().contains("Must contain either a non-null, non-empty 'scopes' property or a non-empty 'presentationDefinition' property.");
     }
 
     @Test
@@ -62,17 +73,16 @@ class PresentationQueryValidatorTest {
                 .inputDescriptors(List.of(InputDescriptor.Builder.newInstance().id(UUID.randomUUID().toString()).constraints(new Constraints(List.of(Field.Builder.newInstance().build()))).build()))
                 .build();
         var jo = createObjectBuilder()
-                .add(PresentationQueryMessage.PRESENTATION_QUERY_MESSAGE_DEFINITION_PROPERTY, createPresentationDefArray(presDef))
+                .add(namespace.toIri(PRESENTATION_QUERY_MESSAGE_DEFINITION_TERM), createPresentationDefArray(presDef))
                 .build();
 
         assertThat(validator.validate(jo)).isSucceeded();
     }
 
-
     @Test
     void validate_withNone_fails() {
         var jo = createObjectBuilder().build();
-        assertThat(validator.validate(jo)).isFailed().detail().contains("Must contain either a 'scope' or a 'presentationDefinition' property.");
+        assertThat(validator.validate(jo)).isFailed().detail().contains("Must contain either a non-null, non-empty 'scopes' property or a non-empty 'presentationDefinition' property.");
     }
 
     @Test
@@ -82,11 +92,11 @@ class PresentationQueryValidatorTest {
                 .inputDescriptors(List.of(InputDescriptor.Builder.newInstance().id(UUID.randomUUID().toString()).constraints(new Constraints(List.of(Field.Builder.newInstance().build()))).build()))
                 .build();
         var jo = createObjectBuilder()
-                .add(PresentationQueryMessage.PRESENTATION_QUERY_MESSAGE_SCOPE_PROPERTY, createScopeArray())
-                .add(PresentationQueryMessage.PRESENTATION_QUERY_MESSAGE_DEFINITION_PROPERTY, createPresentationDefArray(presDef))
+                .add(namespace.toIri(PRESENTATION_QUERY_MESSAGE_SCOPE_TERM), createScopeArray())
+                .add(namespace.toIri(PRESENTATION_QUERY_MESSAGE_DEFINITION_TERM), createPresentationDefArray(presDef))
                 .build();
 
-        assertThat(validator.validate(jo)).isFailed().detail().contains("Must contain either a 'scope' or a 'presentationDefinition', not both.");
+        assertThat(validator.validate(jo)).isFailed().detail().contains("Must contain either a non-null, non-empty 'scopes' property or a non-empty 'presentationDefinition' property, not both.");
     }
 
     private JsonArray createScopeArray() {
